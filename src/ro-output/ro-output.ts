6375b436a251473427995caff9c45eb77a94c9cb
de.toolcreator <de.toolcreator@gmail.com>
2020-11-16 19:11:37 +0100

    Add RO table with times

--- src/ro-output/ro-output.ts
+++ src/ro-output/ro-output.ts
@@ -52,74 +52,117 @@ export class RunningOrderOutput {
     this.root = document.getElementsByClassName('ro-output-content')[0] as HTMLElement;
   }
 
-  private buildRunningOrder(day = this.day): void {
-    if (this.bands.length == 0) {
-      this.root.innerHTML = '<p>There is no running order data yet...</p>';
-      return;
-    }
+  private buildRunningOrder(day = this.day): string {
+    // create table
+    const table = document.createElement('table');
 
-    // create lookup table for band categories
-    const bandCategories: { [name: string]: BandCategory } = {};
-    for (const category of this.bandCategories) {
-      bandCategories[category.name] = category;
+    // add header row
+    const headerRow = document.createElement('tr');
+    const timeTh = document.createElement('th');
+    timeTh.textContent = 'time';
+    headerRow.appendChild(timeTh);
+    for (const stage of this.stages) {
+      const stageTh = document.createElement('th');
+      stageTh.textContent = stage;
+      headerRow.appendChild(stageTh);
     }
+    table.appendChild(headerRow);
 
-    // filter out todays gigs
-    const todaysGigs: { gig: Gig, band: Band, category: BandCategory, clashes: boolean }[] = [];
-    for (const band of this.bands) {
-      for (const gig of band.gigs) {
-        if (gig.start.getFullYear() == day.getFullYear() &&
-          gig.start.getMonth() == day.getMonth() &&
-          gig.start.getDay() == day.getDay()) {
-          todaysGigs.push({ gig: gig, band: band, category: bandCategories[band.category], clashes: false });
-        }
+    if (this.bands.length > 0) {
+      // create lookup table for band categories
+      const bandCategories: { [name: string]: BandCategory } = {};
+      for (const category of this.bandCategories) {
+        bandCategories[category.name] = category;
       }
-    }
 
-    // sort gigs by start time
-    todaysGigs.sort((a, b) => {
-      const aStart = a.gig.start.getTime();
-      const bStart = b.gig.start.getTime();
-      return (aStart < bStart ? -1 : (aStart > bStart ? 1 : 0));
-    });
-
-    // if order of stages not defined yet, find stages
-    if (this.stages.length == 0) {
-      for (const gig of todaysGigs) {
-        const stageName = gig.gig.stage;
-        if (!this.stages.includes(stageName)) {
-          this.stages.push(stageName);
-        }
-      }
-    }
-
-    // check for clashes
-    for (let i = 0; i < todaysGigs.length; i++) {
-      const gig0 = todaysGigs[i];
-      if (gig0.category && gig0.category.clash) {
-        for (let k = i + 1; k < todaysGigs.length; k++) {
-          const gig1 = todaysGigs[k];
-
-          if (gig0.category == gig1.category) {
-            const start0 = gig0.gig.start.getTime();
-            const end0 = gig0.gig.end.getTime();
-            const start1 = gig1.gig.start.getTime();
-            const end1 = gig1.gig.start.getTime();
-
-            const clashes = start0 <= end1 && start1 <= end0;
-            gig0.clashes = clashes;
-            gig1.clashes = clashes;
-          }
-
-          if (gig0.gig.end < gig1.gig.start) {
-            break;
+      // filter out todays gigs
+      const todaysGigs: { gig: Gig, band: Band, category: BandCategory, clashes: boolean }[] = [];
+      for (const band of this.bands) {
+        for (const gig of band.gigs) {
+          if (gig.start.getFullYear() == day.getFullYear() &&
+            gig.start.getMonth() == day.getMonth() &&
+            gig.start.getDay() == day.getDay()) {
+            todaysGigs.push({ gig: gig, band: band, category: bandCategories[band.category], clashes: false });
           }
         }
       }
+
+      if (todaysGigs.length > 0) {
+        // sort gigs by start time
+        todaysGigs.sort((a, b) => {
+          const aStart = a.gig.start.getTime();
+          const bStart = b.gig.start.getTime();
+          return (aStart < bStart ? -1 : (aStart > bStart ? 1 : 0));
+        });
+
+        // if order of stages not defined yet, find stages
+        if (this.stages.length == 0) {
+          for (const gig of todaysGigs) {
+            const stageName = gig.gig.stage;
+            if (!this.stages.includes(stageName)) {
+              this.stages.push(stageName);
+            }
+          }
+        }
+
+        // check for clashes
+        for (let i = 0; i < todaysGigs.length; i++) {
+          const gig0 = todaysGigs[i];
+          if (gig0.category && gig0.category.clash) {
+            for (let k = i + 1; k < todaysGigs.length; k++) {
+              const gig1 = todaysGigs[k];
+
+              if (gig0.category == gig1.category) {
+                const start0 = gig0.gig.start.getTime();
+                const end0 = gig0.gig.end.getTime();
+                const start1 = gig1.gig.start.getTime();
+                const end1 = gig1.gig.start.getTime();
+
+                const clashes = start0 <= end1 && start1 <= end0;
+                gig0.clashes = clashes;
+                gig1.clashes = clashes;
+              }
+
+              if (gig0.gig.end < gig1.gig.start) {
+                break;
+              }
+            }
+          }
+        }
+
+        const startTime = todaysGigs[0].gig.start.getTime();
+        const endTime = todaysGigs[todaysGigs.length - 1].gig.end.getTime();
+
+        const FIVE_MINUTES_IN_MS = 5 * 60 * 1000;
+        for (let curTime = startTime; curTime <= endTime; curTime += FIVE_MINUTES_IN_MS) {
+          // create row
+          const row = document.createElement('tr');
+
+          // create time cell
+          const timeCell = document.createElement('td');
+          const curDate = new Date(curTime);
+          timeCell.textContent = curDate.getHours().toString().padStart(2, '0') + ':' + curDate.getMinutes().toString().padStart(2, '0');
+          row.appendChild(timeCell);
+
+          // TODO
+
+          // add row to table
+          table.appendChild(row);
+        }
+      }
     }
 
+    // remove old table and add new
+    this.root.removeChild(this.root.firstChild);
+    this.root.appendChild(table);
 
+    // serialize table
+    let tableHtml = '<' + table.nodeName.toLowerCase();
+    for (const attribute of table.attributes) {
+      tableHtml += ' ' + attribute.name + '=\'' + attribute.value + '\'';
+    }
+    tableHtml += '>' + table.innerHTML + '</' + table.nodeName.toLowerCase() + '>';
 
-    // TODO
+    return tableHtml;
   }
 }
